ad1d6b021ef04f0c115a14ca95fb8b0343039a21
sudiplun <sudeeplun@gmail.com>
2023-08-03 22:47:18 +0545

    fetch from db and ui change

---
+++ book-products.php
@@ -0,0 +1,76 @@
+<?php
+include('Kisan/includes/config.php');
+
+// Fetch products from the database
+$query = mysqli_query($con, "SELECT * FROM products");
+$products = mysqli_fetch_all($query, MYSQLI_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product List</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <h1 class="my-4">Product List</h1>
+
+        <div class="row">
+            <?php foreach ($products as $product) : ?>
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $product['ProductName']; ?></h5>
+                            <p class="card-text">Price: $<?php echo $product['ProductPrice']; ?></p>
+                            <p class="card-text">Left Quantity: <?php echo $product['LeftQuantity']; ?></p>
+                            <button class="btn btn-primary" data-toggle="modal" data-target="#orderModal<?php echo $product['id']; ?>">Order</button>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Order Modal -->
+                <div class="modal fade" id="orderModal<?php echo $product['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="orderModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="orderModalLabel">Order <?php echo $product['ProductName']; ?></h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <form action="place_order.php" method="post">
+                                    <input type="hidden" name="product_id" value="<?php echo $product['id']; ?>">
+                                    <div class="form-group">
+                                        <label for="customerName">Customer Name</label>
+                                        <input type="text" class="form-control" id="customerName" name="customer_name" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="contact">Contact</label>
+                                        <input type="text" class="form-control" id="contact" name="contact" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="quantity">Quantity</label>
+                                        <input type="number" class="form-control" id="quantity" name="quantity" required>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Submit Order</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /Order Modal -->
+            <?php endforeach; ?>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
--- navbar.php
+++ navbar.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-expand-lg ">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Bari</a>
+        <a class="navbar-brand" href="index.php">
+            <h4>Bari</h4>
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -9,10 +11,7 @@
                 <li class="nav-item">
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="book-products.php">Book products</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">test</a>
+                    <a class="nav-link" href="products.php">products</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -20,8 +19,10 @@
                     </a>
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="Kisan/login.php">Kisan</a></li>
+                        <li><a class="dropdown-item" href="Kisan/dashboard.php">dashboard</a></li>
                     </ul>
-                </li>
+            </ul>
+            </li>
             </ul>
         </div>
     </div>
