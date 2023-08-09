49615bfce58d96c39ca5d1520b4c4f59af12b4a1
sudiplun <sudeeplun@gmail.com>
2023-08-09 21:44:28 +0545

    move to src

--- index.php
+++ index.php
@@ -1,25 +1,4 @@
-<!doctype html>
-<html data-bs-theme="light">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bari kisan</title>
-    <!-- cdn bootstrap css import -->
-    <!-- <link rel=¨stylesheet¨ type=¨text/css¨ href="/bootstrap/dist/css/bootstrap.min.css"> -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-</head>
-
-<body>
-    <?php
-    include_once('navbar.php');
-    ?>
-    <?php
-    include_once('book-products.php');
-    ?>
-    <!-- cdn bootstrap js import -->
-    <!-- <script src="/bootstrap/dist/js/bootstrap.bundle.min.js"></script> -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-</body>
-
-</html>
+<?php
+header("Location: src/home.php")
+?>
+<div>hello you are outside</div>
